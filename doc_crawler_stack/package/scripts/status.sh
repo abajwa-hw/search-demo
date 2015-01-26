f3b271853c34f55ebea72605f13ffd8288f04959
Ali Bajwa <abajwa@hortonworks.com>
2015-01-25 18:39:54 -0800

    moved logic to start, stop, status scripts

--- doc_crawler_stack/package/scripts/status.sh
+++ doc_crawler_stack/package/scripts/status.sh
@@ -2,8 +2,15 @@
 set -e 
 
 #return 1 only if both solr and sbt are up
-RET=(`ps -ef | grep "sb[t]" | wc -l`) && (`ps -ef | grep "start.ja[r]" | wc -l`)
+#RET=[[ `ps -ef | grep "sb[t]" | wc -l` && `ps -ef | grep "start.ja[r]" | wc -l` ]]
+RET1=`ps -ef | grep "sb[t]" | wc -l`
+RET2=`ps -ef | grep "start.ja[r]" | wc -l`
 
-exit $RET
+if [[ $RET1 -gt 0 && $RET2 -gt 0 ]]
+then
+	echo 1
+else 
+	echo 0
+fi
 
 
