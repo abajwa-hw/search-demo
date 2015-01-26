d020b99a5590206f5483f48d029f5e02e38614e4
Ali Bajwa <abajwa@hortonworks.com>
2015-01-26 09:47:51 -0800

    updated setup.sh

--- doc_crawler_stack/package/scripts/setup.sh
+++ doc_crawler_stack/package/scripts/setup.sh
@@ -92,7 +92,7 @@ echo "Completed bower install"
 
 #echo "Starting npm imstall..."
 cd  /root/search-demo/document_crawler/src/main/webapp
-nohup npm install & >> /var/log/doc-crawler.log
+npm install
 echo "Stack installed successfully"
 
 exit 0
