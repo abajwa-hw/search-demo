04852d8bfd93d60ed7bece4f757e59b24afebed3
Ali Bajwa <abajwa@hortonworks.com>
2015-01-28 01:12:40 -0800

    start Solr only if not already running

--- doc_crawler_stack/package/scripts/start_solr.sh
+++ doc_crawler_stack/package/scripts/start_solr.sh
@@ -5,7 +5,13 @@ SOLR_ROOT=$1
 #Logfile e.g. /var/log/doc-crawler.log
 LOGFILE=$2
 
-echo "Starting Solr from $SOLR_ROOT/solr/hdp"
-cd $SOLR_ROOT/solr/hdp
-nohup java -jar start.jar >> $LOGFILE 2>&1 &
-
+#If solr already started, skip the start
+SOLR_PIDS=`ps -ef | grep "start.ja[r]" | wc -l`
+if [ $SOLR_PIDS -eq 0 ]
+then
+	echo "Starting Solr from $SOLR_ROOT/solr/hdp"
+	cd $SOLR_ROOT/solr/hdp
+	nohup java -jar start.jar >> $LOGFILE 2>&1 &
+else
+	echo "Solr already running..."
+fi
