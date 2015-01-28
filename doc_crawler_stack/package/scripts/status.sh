15b01a553904acfdc6f6048e38542a2af84a68f4
Ali Bajwa <abajwa@hortonworks.com>
2015-01-27 21:16:49 -0800

    parameterize zip file

--- doc_crawler_stack/package/scripts/status.sh
+++ doc_crawler_stack/package/scripts/status.sh
@@ -4,7 +4,7 @@ set -e
 #return 1 only if both solr and sbt are up
 #RET=[[ `ps -ef | grep "sb[t]" | wc -l` && `ps -ef | grep "start.ja[r]" | wc -l` ]]
 
-nc -tz localhost 9090
+nc -tz localhost 9090 > /dev/null 2>&1
 #if web server is up, this should return 0
 RET1=$?
 #if sbt is up, this should return > 0
@@ -12,8 +12,8 @@ RET2=`ps -ef | grep "sb[t]" | wc -l`
 #if solr is up this should return > 0
 RET3=`ps -ef | grep "start.ja[r]" | wc -l`
 
-echo "sbt status is $RET2"
-echo "solr status is $RET3"
+#echo "sbt status is $RET2"
+#echo "solr status is $RET3"
 
 # if services are both up...
 if [ $RET1 -eq 0 -a $RET2 -gt 0 -a $RET3 -gt 0 ]
