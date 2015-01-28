492fd4e73ada36e3b7632663f1fb27545c9ffa2e
Ali Bajwa <abajwa@hortonworks.com>
2015-01-27 22:49:16 -0800

    adding seperate solr service

--- doc_crawler_stack/package/scripts/status.sh
+++ doc_crawler_stack/package/scripts/status.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 set -e 
 
-#return 1 only if both solr and sbt are up
-#RET=[[ `ps -ef | grep "sb[t]" | wc -l` && `ps -ef | grep "start.ja[r]" | wc -l` ]]
-
 nc -tz localhost 9090 > /dev/null 2>&1
 #if web server is up, this should return 0
 RET1=$?
 #if sbt is up, this should return > 0
 RET2=`ps -ef | grep "sb[t]" | wc -l`
-#if solr is up this should return > 0
-RET3=`ps -ef | grep "start.ja[r]" | wc -l`
 
-#echo "sbt status is $RET2"
-#echo "solr status is $RET3"
+
 
 # if services are both up...
-if [ $RET1 -eq 0 -a $RET2 -gt 0 -a $RET3 -gt 0 ]
+if [ $RET1 -eq 0 -a $RET2 -gt 0 ]
 then
 	PID1=`ps -ef | grep "sb[t] run" | awk '{print $2}'`
 	echo "Service (pid $PID1) is running..."
--- doc_crawler_stack/package/scripts/stop.sh
+++ doc_crawler_stack/package/scripts/stop.sh
@@ -7,12 +7,6 @@ then
 	ps -ef | grep "sb[t]" | awk '{print $2}' | xargs kill
 fi
 
-if [[ `ps -ef | grep "start.ja[r]" | wc -l` -gt 0 ]] 
-then
-	echo "Stopping Solr"
-	ps -ef | grep "start.ja[r]" | awk '{print $2}' | xargs kill
-fi
-
 echo "Done"
 
 exit 0
