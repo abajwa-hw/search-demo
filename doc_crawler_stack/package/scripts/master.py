ddd8c87cd212eb7c4b1c036d6372e0508627c32e
Ali Bajwa <abajwa@hortonworks.com>
2015-01-28 15:47:58 -0800

    exposing docs dir in config

--- doc_crawler_stack/package/scripts/master.py
+++ doc_crawler_stack/package/scripts/master.py
@@ -20,25 +20,28 @@ class Master(Script):
     Execute('sudo -u hdfs hdfs dfs -chown solr /user/solr')
     Execute('sudo -u hdfs hdfs dfs -chmod -R 777 /user')
     
+
     #e.g. if zipfilepath was set to /root/search-demo/search-docs.zip then this would be /root/search-demo/
-    zipfiledir = os.path.dirname(params.demo_zipfilepath) + os.sep
+    #zipfiledir = os.path.dirname(params.demo_zipfilepath) + os.sep
     #e.g. search-docs.zip
-    zipfilenameext = os.path.basename(params.demo_zipfilepath)
+    #zipfilenameext = os.path.basename(params.demo_zipfilepath)
     #e.g. search-docs
-    zipfilename=os.path.splitext(zipfilenameext)[0]
+    #zipfilename=os.path.splitext(zipfilenameext)[0]
     #e.g. /root/search-demo/search-docs
-    zipdirpath = zipfiledir + zipfilename
+    #zipdirpath = zipfiledir + zipfilename
     
-    Execute('rm -rf '+zipdirpath)
+    #Execute('rm -rf '+zipdirpath)
     #Execute('hadoop fs -rm -R /user/solr/data/rfi_raw/*')
         
-    Execute('cd '+zipfiledir+';unzip '+zipfilenameext+';cd '+zipfilename+';find . -iname \'* *\' -execdir bash -c \'mv "$1" "${1// /_}"\' _ {} \;' )
+    #unzip the sample zip into searchdir and replace all spaces in filename with _    
+    Execute(unzip '+params.demo_samplezip+' -d '+ params.demo_searchdir + ';cd '+zipfilename+';find '+params.demo_searchdir+' -iname \'* *\' -execdir bash -c \'mv "$1" "${1// /_}"\' _ {} \;' )
 
-    Execute ('echo "Move search docs to HDFS"')
-    Execute('hadoop fs -put '+zipdirpath+'/* /user/solr/data/rfi_raw/')
+    Execute ('echo "Copying search docs to HDFS under /user/solr/data/rfi_raw" >> ' + params.stack_log)
+    Execute('hadoop fs -put '+zipdirpath+'/* /user/solr/data/rfi_raw/' + + params.stack_log)
 
 
     #Setup Solr
+    Execute ('echo "Configuring Solr core" >> ' + params.stack_log)    
     Execute('cd '+params.solr_dir+'/solr; cp -r example hdp ; rm -rf hdp/example* hdp/multicore')
     Execute ('cd '+params.solr_dir+'/solr; if [ -d "./hdp/solr/collection1" ]; then rsync -av hdp/solr/collection1/* hdp/solr/rawdocs; else rsync -av hdp/solr/hdp1/* hdp/solr/rawdocs; fi ')
     Execute ('cd '+params.solr_dir+'/solr; rm -f hdp/solr/rawdocs/core.properties ; rm -f hdp/solr/rawdocs/core.properties')
@@ -48,42 +51,42 @@ class Master(Script):
     Execute('/bin/cp -f '+params.demo_dir+'/document_crawler/artifacts/solrconfig.xml  '+params.solr_dir+'/solr/hdp/solr/rawdocs/conf/solrconfig.xml')
     Execute('/bin/cp -f '+params.demo_dir+'/document_crawler/artifacts/schema.xml '+params.solr_dir+'/solr/hdp/solr/rawdocs/conf/schema.xml')
 
-    Execute('echo "Starting Solr"')
+    Execute('echo "Checking if Solr is running...." >>' + params.stack_log)
     Execute(params.stack_dir + '/package/scripts/start_solr.sh ' + params.solr_dir + ' ' + params.stack_log)
     Execute('sleep 10')
     #Create core called rawdocs
     Execute('curl "http://localhost:8983/solr/admin/cores?action=CREATE&name=rawdocs&instanceDir='+params.solr_dir+'/solr/hdp/solr/rawdocs/"')
 
 
-
+    Execute ('echo "Downloading lucidworks jar" >> ' + params.stack_log)    
     Execute('cd /root ; wget http://package.mapr.com/tools/search/lucidworks-hadoop-1.2.0-0-0.tar.gz ; tar xvzf *.tar.gz ; cp lucidworks-hadoop*/hadoop/hadoop-lws-job-*.jar /tmp ; ')
     Execute('echo "starting mapreduce job"  >> ' + params.stack_log)
     Execute('yarn jar /tmp/hadoop-lws-job-1.2.0-0-0.jar com.lucidworks.hadoop.ingest.IngestJob -Dlww.commit.on.close=true -Dadd.subdirectories=true -cls com.lucidworks.hadoop.ingest.DirectoryIngestMapper -c rawdocs -i /user/solr/data/rfi_raw/ -of com.lucidworks.hadoop.io.LWMapRedOutputFormat -s http://sandbox.hortonworks.com:8983/solr')
 
-    Execute('echo "setting up banana"')
+    Execute('echo "setting up banana" >> '  + params.stack_log)
     #Execute('cd /opt/solr ; git clone https://github.com/LucidWorks/banana.git ; rsync -av '+params.solr_dir+'/banana/* '+params.solr_dir+'/solr-4.7.2/hdp/solr-webapp/webapp/')
     Execute('cd /opt/solr ; git clone https://github.com/LucidWorks/banana.git ; mv '+params.solr_dir+'/banana/ '+params.solr_dir+'/solr-4.7.2/hdp/solr-webapp/webapp/')
 
 
-    Execute('echo "change collection1 to rawdocs..."')
+    Execute('echo "change collection1 to rawdocs..." >> '  + params.stack_log)
     Execute('sed -i "s/collection1/rawdocs/g" ' + params.solr_dir + '/solr-4.7.2/hdp/solr-webapp/webapp/banana/src/app/dashboards/default.json')
 
 
-    Execute('echo "Installing sbt, nodejs, npm ..."')
+    Execute('echo "Installing sbt, nodejs, npm ..." >> ' +  params.stack_log)
     Execute('curl https://bintray.com/sbt/rpm/rpm > /root/bintray-sbt-rpm.repo')
     Execute('mv /root/bintray-sbt-rpm.repo /etc/yum.repos.d/')
     Execute('yum install -y sbt nodejs npm >> ' + params.stack_log)
     Execute('echo "Completed sbt, nodejs, npm install"')
     
-    Execute('echo "Starting bower install..."')
+    Execute('echo "Starting bower install..." >> ' + params.stack_log)
     Execute('cd ' + params.demo_dir + '/document_crawler/src/main/webapp' + ' ; ' + \
                                       'npm install -g bower >> ' + params.stack_log + ' ; ' + \
                                       'bower install --allow-root --config.interactive=false ' + params.demo_dir + '/coe-int-master/ >> ' + params.stack_log + ' ; ')
     #Execute('npm install -g bower >> ' + params.stack_log)
     #Execute('bower install --allow-root --config.interactive=false $DEMO_ROOT/coe-int-master/ >> ' + params.stack_log)
-    Execute('echo "Completed bower install"')
+    Execute('echo "Completed bower install" >> '  + params.stack_log)
 
-    Execute('echo "Starting npm imstall..."')
+    Execute('echo "Starting npm imstall..." >> '  + params.stack_log)
     Execute('cd  ' + params.demo_dir + '/document_crawler/src/main/webapp' + ' ;' + \
     									'npm install >> ' + params.stack_log + ' ; ')
 
@@ -91,7 +94,7 @@ class Master(Script):
     #sleep until service is up - first time can take upto 5min
     Execute('RET=1; until [[ $RET -eq 0 ]]; do echo "Sleeping until service is up..." ; sleep 5; nc -tz localhost 9090 > /dev/null 2>&1; RET=$?; done')
 
-    Execute('echo "Stack installed successfully"')
+    Execute('echo "Stack installed and started successfully" >> ' +  params.stack_log)
 
 
   def configure(self, env):
