d6178706338af49f0d20308f9e62d90bdf5d287a
Ali Bajwa <abajwa@hortonworks.com>
2015-01-27 20:48:47 -0800

    parameterize zip file

--- doc_crawler_stack/package/scripts/status.sh
+++ doc_crawler_stack/package/scripts/status.sh
@@ -3,14 +3,20 @@ set -e
 
 #return 1 only if both solr and sbt are up
 #RET=[[ `ps -ef | grep "sb[t]" | wc -l` && `ps -ef | grep "start.ja[r]" | wc -l` ]]
-RET1=`ps -ef | grep "sb[t]" | wc -l`
-RET2=`ps -ef | grep "start.ja[r]" | wc -l`
 
-echo "sbt status is $RET1"
-echo "solr status is $RET2"
+nc -tz localhost 9090
+#if web server is up, this should return 0
+RET1=$?
+#if sbt is up, this should return > 0
+RET2=`ps -ef | grep "sb[t]" | wc -l`
+#if solr is up this should return > 0
+RET3=`ps -ef | grep "start.ja[r]" | wc -l`
+
+echo "sbt status is $RET2"
+echo "solr status is $RET3"
 
 # if services are both up...
-if [[ $RET1 -gt 0 && $RET2 -gt 0 ]]
+if [ $RET1 -eq 0 -a $RET2 -gt 0 -a $RET3 -gt 0 ]
 then
 	PID1=`ps -ef | grep "sb[t] run" | awk '{print $2}'`
 	echo "Service (pid $PID1) is running..."
