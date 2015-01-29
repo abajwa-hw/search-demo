f3730966e086bccec3950add77338939fbe1c15a
Ali Bajwa <abajwa@hortonworks.com>
2015-01-28 17:25:57 -0800

    adding script to regenrate Solr/index

--- regenerate_solr.sh
+++ regenerate_solr.sh
@@ -9,7 +9,7 @@ then
 	echo "Copying search docs from /root/search-demo/search-docs to HDFS under $HDFS_DIR"
 	hadoop fs -put /root/search-demo/search-docs/* $HDFS_DIR
 fi
-echo "Starting indexing of HDFS dir: "
+echo "Starting mapreduce job for indexing of docs in HDFS dir: "
 yarn jar /tmp/hadoop-lws-job-1.2.0-0-0.jar com.lucidworks.hadoop.ingest.IngestJob -Dlww.commit.on.close=true -Dadd.subdirectories=true -cls com.lucidworks.hadoop.ingest.DirectoryIngestMapper -c rawdocs -i $HDFS_DIR -of com.lucidworks.hadoop.io.LWMapRedOutputFormat -s http://sandbox.hortonworks.com:8983/solr
 
 
