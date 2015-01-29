2bfd14b8455ee635a360291ec94786cbe487b986
Ali Bajwa <abajwa@hortonworks.com>
2015-01-28 17:21:46 -0800

    adding script to regenrate Solr/index

--- regenerate_solr.sh
+++ regenerate_solr.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 MODE=$1
-
-if [[ "$MODE"="clean" ]]
+HDFS_DIR="/user/solr/data/rfi_raw/"
+if [ "$MODE" == "clean" ]
 then
-	echo "Cleaning HDFS doc dir: /user/solr/data/rfi_raw"
-	sudo -u hdfs hdfs dfs -rmr /user/solr/data/rfi_raw/*
+	echo "Cleaning HDFS doc dir: $HDFS_DIR"
+	sudo -u hdfs hdfs dfs -rmr $HDFS_DIR/*
+	find /root/search-demo/search-docs -iname '* *' -execdir bash -c 'mv "$1" "${1// /_}"' _ {} \;
+	echo "Copying search docs from /root/search-demo/search-docs to HDFS under $HDFS_DIR"
+	hadoop fs -put /root/search-demo/search-docs/* $HDFS_DIR
 fi
-
-find /root/search-demo/search-docs -iname '* *' -execdir bash -c 'mv "$1" "${1// /_}"' _ {} \;
-echo "Copying search docs from /root/search-demo/search-docs to HDFS under /user/solr/data/rfi_raw"
-hadoop fs -put /root/search-demo/search-docs/* /user/solr/data/rfi_raw/
-yarn jar /tmp/hadoop-lws-job-1.2.0-0-0.jar com.lucidworks.hadoop.ingest.IngestJob -Dlww.commit.on.close=true -Dadd.subdirectories=true -cls com.lucidworks.hadoop.ingest.DirectoryIngestMapper -c rawdocs -i /user/solr/data/rfi_raw/ -of com.lucidworks.hadoop.io.LWMapRedOutputFormat -s http://sandbox.hortonworks.com:8983/solr
+echo "Starting indexing of HDFS dir: "
+yarn jar /tmp/hadoop-lws-job-1.2.0-0-0.jar com.lucidworks.hadoop.ingest.IngestJob -Dlww.commit.on.close=true -Dadd.subdirectories=true -cls com.lucidworks.hadoop.ingest.DirectoryIngestMapper -c rawdocs -i $HDFS_DIR -of com.lucidworks.hadoop.io.LWMapRedOutputFormat -s http://sandbox.hortonworks.com:8983/solr
 
 
